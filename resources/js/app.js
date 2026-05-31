4aa93b2d3b05b8ae1627421ec422b445e63f47a2
pavlo <pashakrasnitskiy@gmail.com>
2026-05-31 10:36:30 +0300

    feat: add scss 7-1 architecture with design tokens

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,4 @@
+import '../scss/app.scss';
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title inertia>{{ config('app.name') }}</title>
-    @vite(['resources/js/app.js', 'resources/css/app.css'])
+    @vite(['resources/js/app.js'])
     @inertiaHead
 </head>
 <body>
